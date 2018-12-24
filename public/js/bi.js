a781ec17887daf986cd601d1c166c9bd830d426f
bichht <bichht95@gmail.com>
2018-12-24 13:49:32 +0700

    up

--- public/js/bi.js
+++ public/js/bi.js
@@ -1,6 +1,35 @@
-$(document).ready(function(){
-    $('[data-toggle="offcanvas"]').click(function(){
+$(document).ready(function () {
+    $('#timelineContent').css("display", "none");
+    $('[data-toggle="offcanvas"]').click(function () {
         $("#navigation").toggleClass("hidden-xs");
     });
- });
+    $('#aboutTab').click(
+        function () {
+            $('#aboutContent').css("display", "block");
+            $('#timelineContent').css("display", "none");
+            $('#skillContent').css("display", "none");
+            $('#saveContent').css("display", "none");
+        });
+    $('#timelineTab').click(
+        function () {
+            $('#aboutContent').css("display", "none");
+            $('#timelineContent').css("display", "block");
+            $('#skillContent').css("display", "none");
+            $('#saveContent').css("display", "none");
+        });
+    $('#skillTab').click(
+        function () {
+            $('#timelineContent').css("display", "none");
+            $('#aboutContent').css("display", "none");
+            $('#skillContent').css("display", "block");
+            $('#saveContent').css("display", "none");
+        });   
+    $('#saveTab').click(
+        function () {
+            $('#timelineContent').css("display", "none");
+            $('#aboutContent').css("display", "none");
+            $('#skillContent').css("display", "none");
+            $('#saveContent').css("display", "block");
+        });  
+});
  
